5c28c3d64a6b42e1ffb70a054f32f39b6ce60e27
paulobarreto27 <184568632+PericlesJunior1212@users.noreply.github.com>
2025-09-01 14:36:14 -0300

    certo

--- modulo02/contOcorre.py
+++ modulo02/contOcorre.py
@@ -2,6 +2,5 @@ lista = [8,9,6,9,3,9,1]
 numero = 9
 quantidade = lista.count(9)
 
- {quantidade} vezes.")
 
-print(f"O número {numero} aparece
+print(f"O número {numero} aparece {quantidade} vezes.")
