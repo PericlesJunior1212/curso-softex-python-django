3a23f6ab212638b4819fcd0c217ca157bf646b77
paulobarreto27 <184568632+PericlesJunior1212@users.noreply.github.com>
2025-09-01 14:34:35 -0300

    fiz uma lista e contei quantas vezes apareceu um elemento dentro dessa lista.

---
+++ modulo02/contOcorre.py
@@ -0,0 +1,7 @@
+lista = [8,9,6,9,3,9,1]
+numero = 9
+quantidade = lista.count(9)
+
+ {quantidade} vezes.")
+
+print(f"O número {numero} aparece
---
+++ modulo02/eleComuns.py
@@ -0,0 +1,6 @@
+lista_1 = ["carro", "bicicleta", "patins", "moto"]
+lista_2 = ["bicicleta", "preto","carro", "marrom"]
+elemento_comuns = [item for item in lista_1 if item in lista_2]
+print(f"Elementos comuns nas listas são {elemento_comuns}")
+
+
