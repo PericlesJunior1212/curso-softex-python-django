f35b72b7e096e1f5d95622c143b4b3d504cfe912
periclesjunior <184568632+PericlesJunior1212@users.noreply.github.com>
2025-09-03 15:41:50 -0300

    peguei somente os login que foram bem sucedido p elo menos uma.

--- modulo02/sistLog.py
+++ modulo02/sistLog.py
@@ -1,1 +1,16 @@
-acessos = [("Pedro", "sucesso")]
+acessos = [("Pedro", "sucesso"), ("Ana","falha"), ("Maria","sucesso"),("Pedro","falha"),("Ana","falha")]
+usuario_sucesso = set()
+usuario_falha = set()
+
+for usuario, status_login in acessos:
+  if status_login == "sucesso" :
+      usuario_sucesso.add(usuario)
+  elif status_login == "falha":
+      usuario_falha.add(usuario)
+  somen_falha = usuario_falha.difference(usuario_sucesso)
+
+print(F"Os usúarios teve pelo menos um login bem-sucedidos foram: {usuario_sucesso}") 
+
+print(f"Esses nao teve o login bem sucedido: {somen_falha}")
+  
+     
