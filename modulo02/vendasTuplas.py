e3cbc0ee892ec272f5a774c880cfb0222c42c5b0
paulobarreto27 <184568632+PericlesJunior1212@users.noreply.github.com>
2025-09-03 14:16:12 -0300

    Usei tuplas e conjutos, vi qual era maio que 100, e peguei apenas o produros

---
+++ modulo02/vendasTuplas.py
@@ -0,0 +1,14 @@
+vendas = [("Teclado", 50,2),("Mouse", 25.50,4),("Monitor", 300, 1), ("Fone", 45, 1),("Webcam", 75.20, 2)]
+produtos_unicos = set()
+vendas_filtradas = []
+for nome_produto, valor, qtd in vendas:
+    mult = valor * qtd
+    if mult >= 100:
+        vendas_filtradas.append((nome_produto, valor, qtd))
+    produtos_unicos.add(nome_produto)
+     
+    
+print(f"As vendas maiores que 100  sao: {vendas_filtradas}")    
+print(f"\nSó os produtos, separados: {produtos_unicos}")
+
+      
