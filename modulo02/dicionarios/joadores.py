bda41e176e7eb10aca601347f74008a88dca4cde
periclesjunior <184568632+PericlesJunior1212@users.noreply.github.com>
2025-09-08 16:02:39 -0300

    Update joadores.py

--- modulo02/dicionarios/joadores.py
+++ modulo02/dicionarios/joadores.py
@@ -1,4 +1,22 @@
-vazia
+jogadores = {"cristiano": 200,"Messi": 198, "Neymar": 450}
+soma = 0
+
+print("--------Opções:---------\n")
+print("1.Atualizar Pontuação do jogador: \n")
+print("2.Adicionar/Pontuar: \n")
+try:
+   escolha = input("Digite sua escolha: ")
+   escolha = int(escolha)
+except ValueError:
+    print("Entrada inválida, apenas do 1 ou 2.")
+   
+if escolha == "1":
+ for jogador, pontos in jogadores:
+   jogador = print("Digite o nome do jogador")
+   pontos = print("Digite quantos pontos irá atualizar")
+   jogador= pontos =+ soma
+   print(soma)
+    
 
     
    
