c704ec5d7674b1a8546f0e61febe9bbdcafe8627
JuanVaz14 <167100614+JuanVaz14@users.noreply.github.com>
2025-09-10 14:15:29 -0300

    introdução ao POO.

---
+++ modulo02/POO/primeiraFuncao.py
@@ -0,0 +1,36 @@
+
+#def diga_oi():
+    #print("Oi! tudo bem?")
+#diga_oi()
+
+# 'nome' é um parâmetro
+#def saudar (nome):
+ #print(f"Olá, {nome}!")
+#Passamos "Maria" para o parâmetro 'nome'
+#saudar ("Maria")
+#saudar ("João")
+
+#def calcular_area_perimetro (base, altura):
+    #area= base * altura
+    #perimetro = 2* (base + altura)
+    #ando a função e recebendo a tupla
+#resultados = calcular_area_perimetro (5, 10)
+# print(f"Resultado como tupla: {resultados}")
+
+#ef calcular_area_perimetro (base, altura):
+    #area = base * altura
+    #perimetro = 2* (base + altura)
+    #return area, perimetro
+# Chamando a função e recebendo a tupla
+#resultados = calcular_area_perimetro (5, 10)
+#print(f"Resultado como tupla: {resultados}")
+
+#def encontrar_par(lista):
+    #for numero in lista:
+     #if numero % 2 == 0:
+        #return numero
+    #return None
+#print(encontrar_par ([1, 3, 5, 6, 8]))
+
+dobrar = lambda x: x * 2
+print(dobrar (5))
