6a02cefa2bbc69c488d202fc9bdf4c821d0b116c
periclesjunior <184568632+PericlesJunior1212@users.noreply.github.com>
2025-09-11 20:00:01 -0300

    coloquei notas como float

--- modulo02/analisNota.py
+++ modulo02/analisNota.py
@@ -1,14 +1,19 @@
 notas = [("Ana","9.5"),("João","8.0"),("Pedro","7.5"),("Ana","10.0"),("Carlos","6.5"),("Maria","10.0")]
-maior = 0
+
+maior = 0.0
 alunos_maior = set()
 alunos_menor = set()
+
 for nome, nota in notas:
-    if maior > notas:
-        maior = notas
-        if nota == maior:
-            alunos_maior.add(nome)
+    nota = float(nota)
+    if nota > maior:
+        maior = nota
+        alunos_maior = {nome}
+    elif nota == maior:
+        alunos_maior.add(nome)
     if nota < 7.0:
         alunos_menor.add(nome)
-        alunos_maior = tuple (alunos_maior)
-print(f"Amaior nota foi: {maior}")
-print(f"Alunos que tiraram maior notas : {alunos_maior}")
+
+print(f"A maior nota foi: {maior}")
+print(f"Alunos que tiraram a maior nota: {alunos_maior}")
+print(f"Alunos com nota menor que 7: {alunos_menor}")
