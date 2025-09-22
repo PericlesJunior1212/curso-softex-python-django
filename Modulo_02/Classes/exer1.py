4dfaa4d3661abc583fcb6e34cbc64e3bad695e51
PericlesJunior <184568632+PericlesJunior1212@users.noreply.github.com>
2025-09-22 17:44:13 -0300

    criando classe

---
+++ Modulo_02/Classes/exer1.py
@@ -0,0 +1,13 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+pessoa1 = Pessoa("João", 25)
+pessoa2 = Pessoa("Maria", 30)
+pessoa3 = Pessoa("Pedro", 17)
+print(pessoa1.nome, pessoa1.idade)
+print(pessoa2.nome, pessoa2.idade)
+print(pessoa3.nome, pessoa3.idade)
+
