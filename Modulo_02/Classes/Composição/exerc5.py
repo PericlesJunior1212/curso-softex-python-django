bf694a27feb1fa24e93d90c46f72727d90bafb6e
PericlesJunior <184568632+PericlesJunior1212@users.noreply.github.com>
2025-09-29 15:41:16 -0300

    listei usando tipagem

---
+++ Modulo_02/Classes/Composição/exerc5.py
@@ -0,0 +1,25 @@
+class Comodo:
+    def __init__(self, nome):
+        self.nome  = nome
+
+class Casa:
+    def __init__(self,list_comodo:list[Comodo]):
+       self.comodos = list_comodo
+    
+    def adicionar_comodo(self,nome_com: Comodo):
+        self.comodos.append(nome_com)
+     
+
+    def listar_comodos(self):
+        for comodo in self.comodos:
+            print(comodo.nome)
+            
+casa = Casa([])
+casa.adicionar_comodo(Comodo("Quarto"))
+
+
+casa.listar_comodos()
+      
+            
+
+
