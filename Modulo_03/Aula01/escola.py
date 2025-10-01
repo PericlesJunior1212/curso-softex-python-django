df96dc4521438843a0f4ec240bcce08c706e5d1a
PericlesJunior1212 <184568632+PericlesJunior1212@users.noreply.github.com>
2025-10-01 15:52:39 -0300

    termina em casa

---
+++ Modulo_03/Aula01/escola.py
@@ -0,0 +1,7 @@
+from estudante import Estudande
+
+class Escola:
+    
+    def __init__(self): 
+        self.lista_estudante = []
+        
--- Modulo_03/Aula01/estudante.py
+++ Modulo_03/Aula01/estudante.py
@@ -6,12 +6,37 @@ class Estudante(Pessoa):
     def __init__(self, nome, idade, matricula):
         super().__init__(nome, idade)
         self.matricula = matricula
-    
+        self._notas = {}
    
     def adicionar_nota(self, materia, nota):
         if materia not in self.notas:
             self.notas[materia] = []
         self.notas[materia].append(nota)
+
+    
+    def mostrar_notas(self):
+        print(f"{self._notas}")
+        
+    
+        @property
+        def notas(self):
+         return self._notas
+    
+        @notas.setter
+        def notas(self, materia, notas):
+        
+         if materia not in self.notas:
+            self._notas[materia] = []
+         self._notas[materia].append[notas]
+         
+estd1 = Estudante(nome="paulo", idade=25, matricula=2365)
+print(Estudante.mostrar_notas(estd1))
+
+        
+        
+        
+    
+    
         
     
 
