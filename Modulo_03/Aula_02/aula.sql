756832903b6c91edc6288c4815aee05910ce67db
PericlesJunior1212 <184568632+PericlesJunior1212@users.noreply.github.com>
2025-10-06 15:30:37 -0300

    imersão em banco de dados.

---
+++ Modulo_03/Aula_02/aula.sql
@@ -0,0 +1,19 @@
+-- Active: 1759769850496@@127.0.0.1@3306
+CREATE TABLE alunos (
+    id integer primary key,
+    nome text not null,
+    idade integer
+);
+
+INSERT INTO alunos (nome, idade) VALUES ('João', 20);
+INSERT INTO alunos (nome, idade) VALUES ('Maria', 28);
+
+SELECT * FROM alunos;
+SELECT nome, idade from alunos;
+
+SELECT * FROM alunos WHERE nome= 'Maria' and idade=28;
+
+UPDATE alunos SET idade = 21 WHERE nome = 'João';
+
+DELETE FROM alunos where nome = 'Maria';
+DELETE FROM alunos where id= 9 or id = 7 or id = 10;
---
+++ Modulo_03/Aula_02/tabelas.sql
@@ -0,0 +1,40 @@
+CREATE TABLE usuarios (
+    id integer primary key,
+    primeiro_nome text not null,
+    sobrenome text not null,
+    email TEXT NOT NULL,
+    senha INT
+);
+
+CREATE TABLE postagens (
+    id integer primary key,
+    titulo text not null,
+    postagem text not null,
+    id_autor INT
+);
+
+INSERT INTO usuarios (primeiro_nome, sobrenome, email, senha) VALUES ('Pericles', 'Junior', 'pericles@gmail.com', 1324);
+
+INSERT INTO usuarios (primeiro_nome, sobrenome, email, senha) VALUES ('Jherika', 'Silva', 'jherika@gmail.com', 1894);
+
+INSERT INTO usuarios (primeiro_nome, sobrenome, email, senha) VALUES ('Gean', 'Carlos', 'gean@gmail.com', 12634);
+
+INSERT INTO usuarios (primeiro_nome, sobrenome, email, senha) VALUES ('Amanda', 'Junior', 'amanda@gmail.com', 4636);
+
+INSERT INTO usuarios (primeiro_nome, sobrenome, email, senha) VALUES ('Carlos', 'Pinto', 'carlos@gmail.com', 4698);
+
+
+INSERT INTO postagens(titulo, postagem, id_autor) VALUES('BadBoy', 'y548j', 4632),
+('Velozes', 'k45663k', 9856),
+('Cara a Cara', 'q4546qh', 8932),
+('Duro de Matar','l4563m', 7854),
+('Spirit', 'u7823k', 2365);
+
+SELECT * FROM usuarios;
+SELECT * FROM postagens;
+
+UPDATE usuarios SET primeiro_nome = 'Carol' WHERE email = 'jherika@gmail.com';
+UPDATE usuarios SET email = 'jherikagatinha@gmail.com' WHERE email = 'jherika@gmail.com';
+UPDATE usuarios SET email = 'carolana@gmail.com' WHERE email = 'jherikagatinha@gmail.com';
+
+DELETE FROM usuarios WHERE id = 2;
