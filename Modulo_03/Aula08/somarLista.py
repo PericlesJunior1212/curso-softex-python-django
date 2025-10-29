35219a61c8717c98eee454c1aa49162e6319b220
Pericles Junior <periclesjunior2022@gmail.com>
2025-10-29 19:09:36 -0300

    vevn

--- Modulo_03/Aula08/somarLista.py
+++ Modulo_03/Aula08/somarLista.py
@@ -1,24 +1,29 @@
 def somar_listas(lista1, lista2):
-  
- 
-  if len(lista1) > len(lista2):
-    lista_maior = lista1
-    lista_menor = lista2
-  else:
-    lista_maior = lista2
-    lista_menor = lista1
+    """Soma duas listas elemento por elemento.
     
-  resultado = []
-  
-  for i in range(len(lista_maior)):
-    try:
-      
-      soma = lista_maior[i] + lista_menor[i]
-      resultado.append(soma)
-    except IndexError:
+    Se as listas tiverem tamanhos diferentes, soma até onde há elementos
+    em ambas e depois mantém os elementos restantes da lista maior.
+    """
+    if len(lista1) > len(lista2):
+        lista_maior = lista1
+        lista_menor = lista2
+    else:
+        lista_maior = lista2
+        lista_menor = lista1
+        
+    resultado = []
+    
+    for i in range(len(lista_maior)):
+        try:
+            soma = lista_maior[i] + lista_menor[i]
+            resultado.append(soma)
+        except IndexError:
+            resultado.append(lista_maior[i])
+            
+    return resultado
 
-      resultado.append(lista_maior[i])
-      
-  return resultado
-
-print(somar_listas(lista1, lista2))
+if __name__ == '__main__':
+    # Código de exemplo
+    lista1 = [1, 2, 3, 4]
+    lista2 = [1, 2]
+    print(somar_listas(lista1, lista2))
--- Modulo_03/Aula08/test_somarLista.py
+++ Modulo_03/Aula08/test_somarLista.py
@@ -1,4 +1,35 @@
 from somarLista import somar_listas
 
-def test_somar_listas():
+
+def test_somar_listas_mesmo_tamanho():
+    """Testa a soma de duas listas do mesmo tamanho."""
+    lista1 = [1, 2, 3]
+    lista2 = [4, 5, 6]
+    resultado_esperado = [5, 7, 9]  # 1+4, 2+5, 3+6
     
+    assert somar_listas(lista1, lista2) == resultado_esperado
+
+def test_somar_listas_tamanhos_diferentes_primeira_maior():
+    """Testa quando a primeira lista é maior que a segunda."""
+    lista1 = [1, 2, 3, 4]
+    lista2 = [1, 2]
+    resultado_esperado = [2, 4, 3, 4]  # soma até onde tem par, depois mantém valores da maior
+    
+    assert somar_listas(lista1, lista2) == resultado_esperado
+
+def test_somar_listas_tamanhos_diferentes_segunda_maior():
+    """Testa quando a segunda lista é maior que a primeira."""
+    lista1 = [1, 2]
+    lista2 = [4, 5, 6, 7]
+    resultado_esperado = [5, 7, 6, 7]  # soma até onde tem par, depois mantém valores da maior
+    
+    assert somar_listas(lista1, lista2) == resultado_esperado
+
+def test_somar_listas_vazia():
+    """Testa quando uma das listas está vazia."""
+    lista1 = []
+    lista2 = [1, 2, 3]
+    resultado_esperado = [1, 2, 3]  # deve retornar a lista não-vazia
+    
+    assert somar_listas(lista1, lista2) == resultado_esperado
+    assert somar_listas(lista2, lista1) == resultado_esperado  # ordem não deve importar
