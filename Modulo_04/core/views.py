d89de1a4f2ffd62aa584f60ee4af8f4c54b7ca7f
Pericles Junior <periclesjunior2022@gmail.com>
2025-11-08 13:54:30 -0300

    corrigido, ois nao estava corretos.

--- Modulo_04/core/views.py
+++ Modulo_04/core/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
-# Uma 'view' é uma função que recebe um 'request' e retorna uma 'response'
+# Create your views here.
 def home(request):
-
-    # Vamos retornar a resposta HTTP mais simples: um texto HTML
-    return HttpResponse("<h1 style='color: green;'>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    context = {
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
+    }
+    # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    return render(request, 'home.html', context)
 
 def segundo(request):
-
-    return HttpResponse("<h3 style='color: orange;'>Olá, Mundo! Esta é minha segunda página Django!</h3>")
+    return HttpResponse("<input>Login</input>")
