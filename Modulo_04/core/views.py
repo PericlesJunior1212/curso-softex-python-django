a923550ddeb4935382fda4788ee679bb5e6339e9
PericlesJunior1212 <184568632+PericlesJunior1212@users.noreply.github.com>
2025-11-10 15:36:48 -0300

    corrigi, parte 4 com erro

--- Modulo_04/core/views.py
+++ Modulo_04/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Tarefa
 # Create your views here.
 def home(request):
     context = {
@@ -9,5 +9,13 @@ def home(request):
     # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
     return render(request, 'home.html', context)
 
-def segundo(request):
-    return HttpResponse("<input>Login</input>")
+def home(request):
+
+    todas_as_tarefas = Tarefa.objects.all()
+# 3. Atualize o contexto
+    context = {
+    'nome_usuario': 'Júnior',
+    'tecnologias': ['Python', 'Django', 'Models', 'Admin'],
+    'tarefas': todas_as_tarefas # 4. Adicione as tarefas ao contexto
+    }
+    return render(request, 'home.html', context)
