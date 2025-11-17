09842c1719d754c2da88d9345dce9f9f5b33822f
Ana Araujo <184568632+PericlesJunior1212@users.noreply.github.com>
2025-11-17 15:32:11 -0300

    adicionei urls para conexão com a home e views

--- Modulo_04/core/urls.py
+++ Modulo_04/core/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # Quando a URL for a raiz (''), chame a função 'home' de 'views.py'
     path('home', views.home, name='home'),
     path('segundo/', views.segundo, name='segundo'),
+    path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
 ]
