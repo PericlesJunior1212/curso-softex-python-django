0309e4a55720875d100cc10cb61cf7a09c3acf1a
PJ <184568632+PericlesJunior1212@users.noreply.github.com>
2025-11-19 15:11:02 -0300

    add para o user quando removido, delete tudo.

--- Modulo_04/core/models.py
+++ Modulo_04/core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 class Tarefa(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     titulo = models.CharField(max_length=200)
 
