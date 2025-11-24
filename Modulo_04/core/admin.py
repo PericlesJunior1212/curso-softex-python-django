1dc3202d2f71691bb6c854429a937a320509852f
PericlesJunior1212 <184568632+PericlesJunior1212@users.noreply.github.com>
2025-11-24 15:12:38 -0300

    adicionei mas tarefas no admin do site

--- Modulo_04/core/admin.py
+++ Modulo_04/core/admin.py
@@ -1,4 +1,29 @@
+# core/admin.py
+
 from django.contrib import admin
-from .models import Tarefa # 1. Importe seu Model
-    # 2. "Registre" seu Model no site de administração
-admin.site.register(Tarefa)
+from .models import Tarefa
+class TarefaAdmin(admin.ModelAdmin):
+
+    # 'list_display' é uma tupla com os nomes dos campos
+    # que queremos exibir como colunas na lista
+    list_display = ('titulo', 'user', 'get_user_email', 'concluida', 'criada_em')
+    list_filter = ('concluida', 'user', 'criada_em')
+    search_fields = ('titulo', 'user__username')
+    
+    
+    fieldsets = (
+         ('Informações Principais', {
+        'fields': ('user', 'titulo')
+         }),
+         ('Status da Tarefa', {
+         'fields': ('concluida', 'criada_em')
+         }),
+)
+    
+    readonly_fields = ('criada_em',)
+    
+    @admin.display(description='Email do Usuário') # Define o título da coluna
+    def get_user_email(self, obj):
+        return obj.user.email
+
+admin.site.register(Tarefa,TarefaAdmin)
