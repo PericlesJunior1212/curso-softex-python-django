9b0aee31080886d77470c60aac04933a84c1fbf9
Pericles Junior <periclesjunior2022@gmail.com>
2025-11-25 19:52:13 -0300

    corrigindo

--- Modulo_04/core/models.py
+++ Modulo_04/core/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 class Tarefa(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    
+    # 2. ADICIONE A NOVA RELAÇÃO
+    # Cada Tarefa agora DEVE estar ligada a um Projeto.
+    # Usamos uma referência por string para evitar import circular entre apps
+    # formato: 'app_label.ModelName'
+    projects = models.ForeignKey('projects.Project', on_delete=models.CASCADE, related_name='tarefas')
 
     titulo = models.CharField(max_length=200)
 
